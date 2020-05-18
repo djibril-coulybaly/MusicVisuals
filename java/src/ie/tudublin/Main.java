57929c8e848f6aa8e9136f4c542d22d3766c98d5
Djibril Coulybaly <46174096+djibril-coulybaly@users.noreply.github.com>
2020-05-18 23:22:30 +0100

    Added links to sketches (Un-comment in main to run sketches))

--- java/src/ie/tudublin/Main.java
+++ java/src/ie/tudublin/Main.java
@@ -3,6 +3,8 @@ package ie.tudublin;
 import example.CubeVisual;
 import example.MyVisual;
 
+import c18423664.*;
+
 public class Main
 {	
 
@@ -12,9 +14,44 @@ public class Main
         processing.core.PApplet.runSketch( a, new CubeVisual());		
 	}
 
+	public void intro()
+	{
+		String[] a = {"MAIN"};
+		processing.core.PApplet.runSketch(a, new intro());
+	}
+
+	public void flags()
+	{
+		String[] a = {"MAIN"};
+		processing.core.PApplet.runSketch(a, new Flags());
+	}
+
+	public void japaneseMountain()
+	{
+		String[] a = {"MAIN"};
+		processing.core.PApplet.runSketch(a, new japaneseMountain());
+	}
+
+	public void SpinningLocusFlower()
+	{
+		String[] a = {"MAIN"};
+		processing.core.PApplet.runSketch(a, new SpinningLocusFlower());
+	}
+
+	public void Crane()
+	{
+		String[] a = {"MAIN"};
+		processing.core.PApplet.runSketch(a, new Crane());
+	}
+
 	public static void main(String[] args)
 	{
 		Main main = new Main();
-		main.startUI();			
+		//main.startUI();	
+		main.intro();	
+		//main.flags();
+		// main.SpinningLocusFlower();
+		// main.Crane();
+		// main.japaneseMountain();	
 	}
 }
